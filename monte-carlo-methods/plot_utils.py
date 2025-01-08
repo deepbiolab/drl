587cbd242afd454912b6268d1b0e18673ed25894
deepbiolab <lin_xiangtian@outlook.com>
2025-01-08 23:09:47 +0800

    Add First Visit Constant α GLIE MC Control

--- monte-carlo-methods/plot_utils.py
+++ monte-carlo-methods/plot_utils.py
@@ -33,36 +33,57 @@ def plot_blackjack_values(V):
     get_figure(False, ax)
     plt.show()
 
+
 def plot_policy(policy):
 
     def get_Z(x, y, usable_ace):
-        if (x,y,usable_ace) in policy:
-            return policy[x,y,usable_ace]
+        if (x, y, usable_ace) in policy:
+            return policy[x, y, usable_ace]
         else:
             return 1
 
     def get_figure(usable_ace, ax):
         x_range = np.arange(11, 22)
-        y_range = np.arange(10, 0, -1)
+        y_range = np.arange(1, 11)
         X, Y = np.meshgrid(x_range, y_range)
-        Z = np.array([[get_Z(x,y,usable_ace) for x in x_range] for y in y_range])
-        surf = ax.imshow(Z, cmap=plt.get_cmap('Pastel2', 2), vmin=0, vmax=1, extent=[10.5, 21.5, 0.5, 10.5])
-        plt.xticks(x_range)
-        plt.yticks(y_range)
-        plt.gca().invert_yaxis()
-        ax.set_xlabel('Player\'s Current Sum')
-        ax.set_ylabel('Dealer\'s Showing Card')
+        Z = np.array([[get_Z(x, y, usable_ace) for x in x_range] for y in y_range])
+        
+        surf = ax.imshow(
+            Z.T,
+            cmap=plt.get_cmap('Pastel2', 2),
+            vmin=0,
+            vmax=1,
+            extent=[0.5, 10.5, 10.5, 21.5],
+            origin='lower'
+        )
+        
+        ax.set_xticks(np.arange(1, 11))
+        ax.set_yticks(np.arange(11, 22))
+        ax.set_xticklabels(np.arange(1, 11,))
+        ax.set_yticklabels(np.arange(11, 22))
+
+        ax.set_xlabel('Dealer\'s Showing Card')
+        ax.set_ylabel('Player\'s Current Sum')
+
         ax.grid(color='w', linestyle='-', linewidth=1)
+
+        # Add color bar
         divider = make_axes_locatable(ax)
         cax = divider.append_axes("right", size="5%", pad=0.1)
-        cbar = plt.colorbar(surf, ticks=[0,1], cax=cax)
-        cbar.ax.set_yticklabels(['0 (STICK)','1 (HIT)'])
-            
+        cbar = plt.colorbar(surf, ticks=[0, 1], cax=cax)
+        cbar.ax.set_yticklabels(['0 (STICK)', '1 (HIT)'])
+
+    # Create figure
     fig = plt.figure(figsize=(15, 15))
+    
+    # Usable Ace
     ax = fig.add_subplot(121)
     ax.set_title('Usable Ace')
     get_figure(True, ax)
+    
+    # No Usable Ace
     ax = fig.add_subplot(122)
     ax.set_title('No Usable Ace')
     get_figure(False, ax)
+    
     plt.show()
